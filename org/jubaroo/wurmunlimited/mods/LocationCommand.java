11c6aa44d28d2d00edfdee5eef4a78655d931dd0
Jubaroo <Jubaroo2@gmail.com>
2017-11-05 17:51:17 -0500

    Errors Fixed

--- org/jubaroo/wurmunlimited/mods/LocationCommand.java
+++ org/jubaroo/wurmunlimited/mods/LocationCommand.java
@@ -1,3 +1,31 @@
+
+package org.jubaroo.wurmunlimited.mods;
+
+import com.wurmonline.server.creatures.Communicator;
+import com.wurmonline.server.creatures.Creature;
+import com.wurmonline.server.players.Player;
+import org.gotti.wurmunlimited.modloader.interfaces.PlayerMessageListener;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
+
+
+public class LocationCommand implements WurmServerMod, PlayerMessageListener {
+
+    private static void handleSlashMessageLocation(Communicator chat, final Creature performer, final int tilex, final int tiley) {
+        chat.sendNormalServerMessage("That tile is at " + tilex + ", " + tiley + ", are you on the surface? =" + performer.isOnSurface());
+    }
+
+    @Override
+    public boolean onPlayerMessage(Communicator communicator, String message) {
+        if (message.startsWith("/location")) {
+            final Player player = communicator.getPlayer();
+            handleSlashMessageLocation(communicator, player, player.getTileX(), player.getTileY());
+            return true;
+        }
+        return false;
+    }
+}
+
+/*
 package org.jubaroo.wurmunlimited.mods;
 
 import com.wurmonline.server.creatures.Communicator;
@@ -24,3 +52,5 @@ public class LocationCommand implements WurmServerMod, PlayerMessageListener {
         return false;
     }
 }
+*/
+
